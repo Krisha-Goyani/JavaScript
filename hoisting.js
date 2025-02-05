bc37f73a1411300043465b6633f7e5a53eb9e39e
Krisha Goyani <krishagoyani21@gmail.com>
2025-02-05 07:54:40 +0530

    fourth-1 commit

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,17 @@
+// getName();
+// console.log(getName);
+
+// console.log(x);  // undefined
+var x = 7;
 function getName(){
     console.log("Hello")
 }
+
+var getName2 = () => {   //behaves like an variable
+    console.log("heyy");
+}
+
+console.log(x);
 getName();
 console.log(getName);
-
-// console.log(x);
-var x = 7;
-console.log(x);
 
